32872ded88571551affccc5b8bd7a082b4c2742e
Chris Joakim <christopher.joakim@gmail.com>
2024-06-04 10:16:20 -0400

    deleted the cpg/ directory, is now pg/

--- cpg/python/psql/extensions.sql
+++
@@ -1,7 +0,0 @@
--- extensions.sql
--- query the installed PostgreSQL extensions
--- Chris Joakim, Microsoft 
-
--- SELECT CREATE_EXTENSION('vector');  <-- enables pgvector
-
-select * FROM pg_extension order by extname;
--- cpg/python/psql/query_players.sql
+++
@@ -1,5 +0,0 @@
--- query_players.sql
--- query a few of the primary example players in the database
--- Chris Joakim, Microsoft 
-
-select * from players where player_id in ('aaronha01', 'rosepe01', 'jeterde01', 'henderi01', 'blombro01', 'guidrro01')
